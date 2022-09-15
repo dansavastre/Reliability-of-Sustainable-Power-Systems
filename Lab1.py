f6ed6638a898cbf618bccf789e2b35e9d70c6b60
Dan <D.Savastre@student.tudelft.nl>
2022-09-15 15:50:19 +0200

    Save all figures

--- Lab1.py
+++ Lab1.py
@@ -48,10 +48,38 @@ def assignment_2(name, nfails, km, nyears, mean):
 
     print("Confidence interval for {} = [{}, {}]".format(name, confidence_interval_left, confidence_interval_right))
 
-    plt.boxplot([confidence_interval_left, mean, confidence_interval_right])
-    plt.show()
-    plt.close()
+    # figure related code
+    fig = plt.figure()
+    fig.suptitle('Assignment 2', fontsize=14, fontweight='bold')
+    if name.find("5") == -1:
+        fig.suptitle('Assignment 3', fontsize=14, fontweight='bold')
 
+    ax = fig.add_subplot(111)
+    ax.boxplot([confidence_interval_left, mean, confidence_interval_right])
+
+    ax.set_title('Failure frequency for {}'.format(name))
+    ax.set_xlabel('failure')
+    ax.set_ylabel('frequency')
+
+    fig.savefig('Assignment 2 - Failure frequency for {}.png'.format(name))
+    plt.show()
+
+def assignment_3():
+    # 3 years later
+    # 42 EHV OHL faliures
+    # 167 HV OHL faliures
+    # 2471 km of EHV OHL
+    # 4078 km of HV OHL
+
+    # Calculate the failure frequencies of EHV, HV, and EHV/HV OHLs again, together with their
+    # 95% confidence intervals. Also plot the results as a boxplot
+    print("\n\nAssignment 3:\n")
+
+    mean_EHV_3 = assignment_1('EHV-8years', 42, 2471, 8)
+    mean_HV_3 = assignment_1('HV-8years', 167, 4078, 8)
+
+    assignment_2('EHV-8years', 42, 2471, 3, mean_EHV_3)
+    assignment_2('HV-8years', 167, 4078, 3, mean_HV_3)
 def assignment_4():
     print("\n\nAssignment 4:\n");
     df = pd.read_csv("repair_times_OHL.csv")
@@ -62,12 +90,23 @@ def assignment_4():
     print("Maximum repair time = {}".format(max(data)))
 
 def assignment_5():
-    print("\n\nAssignment 5:\n");
+    #print("\n\nAssignment 5:\n");
     df = pd.read_csv("repair_times_OHL.csv")
     data = df["Repair Times"].values.tolist()
-    plt.boxplot(data)
+
+    # figure related code
+    fig = plt.figure()
+    fig.suptitle('Assignment 5', fontsize=14, fontweight='bold')
+
+    ax = fig.add_subplot(111)
+    ax.boxplot(data)
+
+    ax.set_title('Repair times for OHL')
+    ax.set_xlabel('repair')
+    ax.set_ylabel('time')
+
+    fig.savefig('Assignment 5.png')
     plt.show()
-    plt.close()
 
 def assignment_6():
     print("\n\nAssignment 6:\n")
@@ -85,8 +124,20 @@ def assignment_6():
     Ft = np.arange(1.0/(len(data)+1), 1.00001-1.0/(len(data)+1), 1/(len(data) + 1))
     x_axis = np.arange(0, 70 + 0.00001, 0.1)
     params, covariance = curve_fit(exponential, data, Ft)
-    plt.scatter(data, Ft)
-    plt.plot(x_axis, exponential(x_axis, *params), '--')
+
+    # figure related code
+    fig = plt.figure()
+    fig.suptitle('Assignment 6', fontsize=14, fontweight='bold')
+
+    ax = fig.add_subplot(111)
+    ax.scatter(data, Ft)
+    ax.plot(x_axis, exponential(x_axis, *params), '--')
+
+    ax.set_title('Fitted exponential curve')
+    ax.set_xlabel('repair time')
+    ax.set_ylabel('F(t)')
+
+    fig.savefig('Assignment 6 - Fitted exponential curve.png')
     plt.show()
     print(params)
 
@@ -97,39 +148,23 @@ def exponential(b, t):
     return -np.exp(-b * t) + 1
 
 if __name__ == '__main__':
-    assignment_6()
+    print("Assignment 1:\n")
+    mean_EHV = assignment_1('EHV', 25, 2310, 5)
+    mean_HV = assignment_1('HV', 51, 3329, 5)
+    mean_EHV_HV = calculate_mean(25 + 51, 2310 + 3329, 5)
+    if mean_EHV > mean_HV:
+        print("EHV has a higher failure frequency than HV\n\n")
+    else:
+        print("HV has a higher failure frequency than EHV\n\n")
 
-# if __name__ == '__main__':
-#     print("Assignment 1:\n")
-#     mean_EHV = assignment_1('EHV', 25, 2310, 5)
-#     mean_HV = assignment_1('HV', 51, 3329, 5)
-#     if mean_EHV > mean_HV:
-#         print("EHV has a higher failure frequency than HV\n\n")
-#     else:
-#         print("HV has a higher failure frequency than EHV\n\n")
-#
-#     print("Assignment 2:\n")
-#     assignment_2('EHV', 25, 2310, 5, mean_EHV)
-#     assignment_2('HV', 51, 3329, 5, mean_HV)
-#     mean_EHV_HV = calculate_mean(25 + 51, 2310 + 3329, 5)
-#     assignment_2('EHV/HV', 25 + 51, 2310 + 3329, 5, mean_EHV + mean_HV)
-#
-#     # 3 years later
-#     # 42 EHV OHL faliures
-#     # 167 HV OHL faliures
-#     # 2471 km of EHV OHL
-#     # 4078 km of HV OHL
-#
-#     # Calculate the failure frequencies of EHV, HV, and EHV/HV OHLs again, together with their
-#     # 95% confidence intervals. Also plot the results as a boxplot
-#     print("\n\nAssignment 3:\n")
-#
-#     mean_EHV_3 = assignment_1('EHV', 42, 2471, 8)
-#     mean_HV_3 = assignment_1('HV', 167, 4078, 8)
-#
-#     assignment_2('EHV', 42, 2471, 3, mean_EHV_3)
-#     assignment_2('HV', 167, 4078, 3, mean_HV_3)
-#
-#     assignment_4()
+    print("Assignment 2:\n")
+    assignment_2('EHV-5years', 25, 2310, 5, mean_EHV)
+    assignment_2('HV-5years', 51, 3329, 5, mean_HV)
+    assignment_2('EHV-HV-5years', 25 + 51, 2310 + 3329, 5,mean_EHV_HV)
+
+    assignment_3()
+    assignment_4()
+    assignment_5()
+    assignment_6()
 
 
